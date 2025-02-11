7a882672b0b8e75a03cc0e87f8df204d09ceaee3
Maciej Romanowski <neipreis22@gmail.com>
2025-02-11 22:05:44 +0100

    added common loader

---
+++ web/src/components/CommonLoader.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-progress-circular
+    color="teal"
+    indeterminate
+    :size="70"
+    :width="7"
+    class="common-loader"
+  />
+</template>
+
+<style lang="scss">
+.common-loader {
+  display: flex;
+  margin: 50% auto 0;
+}
+</style>
